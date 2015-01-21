df1cd1324f3f01e8cea2aed9e05f49004c68d3f0
Ronny Chan <ronny6993@gmail.com>
2015-01-20 23:08:19 -0500

    Emulator-SnowflakeRetroArch: Initial Commit with only BSNESCompatibility core

---
+++ Emulator-SnowflakeRetroarch/SnowflakeRA.bSNES-compatibility/EmulatorBSNESCompatibility.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel.Composition;
+using System.Reflection;
+using Snowflake.Emulator;
+using Snowflake.Emulator.Configuration;
+using Snowflake.Emulator.Input;
+using Snowflake.Game;
+using Snowflake.Platform;
+using Snowflake.Service;
+
+
+namespace SnowflakeRA.bSNEScompatibility
+{
+    public class EmulatorBSNESCompatibility : EmulatorBridge
+    {
+        [ImportingConstructor]
+        public EmulatorBSNESCompatibility([Import("coreInstance")] ICoreService coreInstance)
+            : base(Assembly.GetExecutingAssembly(), coreInstance)
+        {
+        }
+
+        private static readonly string retroArchConfigTemplate = "RetroArchCore";
+        private static readonly string retroArchInputTemplateTemplate = "RetroArchInput";
+
+        public override void StartRom(IGameInfo game)
+        {
+
+        }
+        public override void ShutdownEmulator()
+        {
+
+        }
+        public override void HandlePrompt(string promptMessage)
+        {
+
+        }
+    }
+}
